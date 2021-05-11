da098402d0807f28551b152ab0e5a473518b0d5f
zhangjie0524 <1224649136@qq.com>
2021-05-11 14:14:41 +0800

    signal

--- multi_proc_basis.c
+++ multi_proc_basis.c
@@ -4,22 +4,50 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <wait.h>
 #include <fcntl.h>
 
+int procCount = 1, procMax;
+int fd1_td1, fd2_td1, fd1_td2, fd2_td2;
+size_t count = 5;
+char buffer[5];
+pid_t pid;
+
+
+int createChild() {
+    if(procCount > procMax) {
+        return 0;
+    }
+    procCount++;
+    pid = fork();
+    if(pid < 0) {
+        printf("error");
+    }else if(pid == 0) {
+        printf("[pid](%d) [ppid](%d): start\n", getpid(), getppid());
+        read(fd1_td1, buffer, count);
+        printf("[pid](%d) [ppid](%d): [read](%s) by [fd1_td1](%d)\n", getpid(), getppid(), buffer, fd1_td1);
+        fd2_td1 = open("td1.txt", O_RDONLY);
+        printf("[pid](%d) [ppid](%d): [fd2_td1](%d) is got\n", getpid(), getppid(), fd2_td1);
+        read(fd2_td1, buffer, count);
+        printf("[pid](%d) [ppid](%d): [read](%s) by [fd2_td1](%d)\n", getpid(), getppid(), buffer, fd2_td1);
+        close(fd2_td1);
+        close(fd1_td1);
+        printf("[pid](%d) [ppid](%d): exit\n", getpid(), getppid());
+        exit(0);
+    }else {
+        printf("[pid](%d) [ppid](%d): startParen\n", getpid(), pid);
+        createChild();
+    }
+}
 
 int main(int argc, char *argv[]) {
-     int procCount;
-     int fd1_td1, fd2_td1, fd1_td2, fd2_td2;
-     size_t count = 5;
-     char buffer[5];
-     pid_t pid;
 
     if(strcmp("-c", argv[1]) == 0) {
         if(atoi(argv[2]) > 6) {
             printf("No more than 6 process!\n");
             return 0;
         }else {
-            procCount = atoi(argv[2]);
+            procMax = atoi(argv[2]);
         }
     }
     //启动
@@ -28,27 +56,37 @@ int main(int argc, char *argv[]) {
     fd1_td1 = open("td1.txt", O_RDONLY);
     printf("[pid](%d) [ppid](%d): [fd1_td1](%d) is got\n",getpid(),getppid(), fd1_td1);
 
-    for(int i = 0; i < procCount - 1; i++) {
-            pid = fork();
-            if(pid < 0) {
-                printf("error");
-            }else if(pid == 0) {
-                printf("[pid](%d) [ppid](%d): start\n", getpid(), getppid());
-                read(fd1_td1, buffer, count);
-                printf("[pid](%d) [ppid](%d): [read](%s) by [fd1_td1](%d)\n", getpid(), getppid(), buffer, fd2_td1);
-                fd2_td1 = open("td1.txt", O_RDONLY);
-                printf("[pid](%d) [ppid](%d): [fd2_td1](%d) is got\n", getpid(), getppid(), fd2_td1);
-                read(fd2_td1, buffer, count);
-                printf("[pid](%d) [ppid](%d): [read](%s) by [fd2_td1](%d)\n", getpid(), getppid(), buffer, fd2_td1);
-                close(fd2_td1);
-                close(fd1_td1);
-                printf("[pid](%d) [ppid](%d): exit\n", getpid(), getppid());
-                exit(0);
-            }else {
-                // wait(NULL);
-                sleep(2);
-                printf("[pid](%d) [ppid](%d): ", getpid(), pid);
-            }
+    
+    for(int i = 0; i < procMax; i++) {
+        pid = fork();
+        if(pid < 0) {
+            printf("error");
+        }else if(pid == 0) {
+            printf("[pid](%d) [ppid](%d): start\n", getpid(), getppid());
+            read(fd1_td1, buffer, count);
+            printf("[pid](%d) [ppid](%d): [read](%s) by [fd1_td1](%d)\n", getpid(), getppid(), buffer, fd1_td1);
+            fd2_td1 = open("td1.txt", O_RDONLY);
+            printf("[pid](%d) [ppid](%d): [fd2_td1](%d) is got\n", getpid(), getppid(), fd2_td1);
+            read(fd2_td1, buffer, count);
+            printf("[pid](%d) [ppid](%d): [read](%s) by [fd2_td1](%d)\n", getpid(), getppid(), buffer, fd2_td1);
+            close(fd2_td1);
+            close(fd1_td1);
+            printf("[pid](%d) [ppid](%d): exit\n", getpid(), getppid());
+            exit(0);
+        }else {
+            read(fd1_td1, buffer, count);
+            printf("[pid](%d) [ppid](%d): [read](%s) by [fd1_td1](%d)\n", getpid(), getppid(), buffer, fd1_td1);
+            close(fd1_td1);
+
+            fd1_td2 = open("td2.txt", O_RDONLY);
+            printf("[pid](%d) [ppid](%d): [fd1_td2](%d) is got\n", getpid(), getppid(), fd1_td2);
+            read(fd1_td2, buffer, count);
+            printf("[pid](%d) [ppid](%d): [read](%s) by [fd1_td2](%d)\n", getpid(), getppid(), buffer, fd1_td2);
+            close(fd1_td2);
+        }
     }
-    return 0;
+        sleep(2);
+        printf("[pid](%d) [ppid](%d): exit\n", getpid(), getppid());
+        exit(0);
+        return 0;
 }
---
+++ signal_basis.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+// #include <string.h>
+
+static void sigHandler(int sig) {
+    if(sig == SIGINT) {
+        printf("[pid](%d): [sig](SIGINT) is captured\n", getpid());
+        printf("[pid](%d): exit\n", getpid());
+        exit(0);
+    }
+
+    if(sig == SIGUSR1) {
+        printf("[pid](%d): [sig](SIGUSR1) is catured\n", getpid());
+    }
+
+    if(sig == SIGUSR2) {
+        printf("[pid](%d): [sig](SIGUSR2) is captured\n", getpid());
+    }
+}
+
+
+int main() {
+
+    printf("[pid](%d): start\n", getpid());
+    // if(signal(SIGINT, sigHandler) == SIG_ERR) {
+    //     printf("error\n");
+    // }
+
+    // if(signal(SIGUSR1, sigHandler) == SIG_ERR) {
+    //     printf("error\n");
+    // }
+
+    // if(signal(SIGUSR2, sigHandler) == SIG_ERR) {
+    //     printf("error\n");
+    // }
+
+    //初始化
+    struct sigaction act;
+    memset(&act, 0, sizeof(act));
+    sigemptyset(&act.sa_mask);
+    act.sa_handler = sigHandler;
+
+    //安装信号处理器
+    if(sigaction(SIGINT, &act, NULL) == -1) {
+        printf("error\n");
+    }
+
+    if(sigaction(SIGUSR1, &act, NULL) == -1) {
+        printf("error\n");
+    }
+
+    if(sigaction(SIGUSR2, &act, NULL) == -1) {
+        printf("error\n");
+    }
+
+    //等待信号
+    while(1){
+        pause();
+    }
+}
